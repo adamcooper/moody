34bd6ed0a2f7c9eb1f6dbaf3ac01592d811f893f
Adam Cooper <adam.cooper@gmail.com>
2014-10-24 15:18:56 -0700

    add a second request spec

--- moody-server/spec/request/emotions_spec.rb
+++ moody-server/spec/request/emotions_spec.rb
@@ -4,10 +4,15 @@ RSpec.describe "Emotions", type: :request do
 
   describe "listing" do
     it "is successful" do
-      FactoryGirl.create(:emotion, name: 'happy')
-
       get '/emotions'
       expect(response).to be_success
+    end
+
+    it "returns a json of the emotion" do
+      FactoryGirl.create(:emotion, name: 'happy')
+      get '/emotions'
+      expect(json.length).to eq(1)
+      expect(json[0]["name"]).to eq('happy')
     end
   end
 
